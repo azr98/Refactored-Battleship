80c33c15c864df44ebdc4338aaec82ab1ef46f18
azr98 <azhar981@gmail.com>
2022-11-19 12:12:40 +0000

    Empty input on button click

--- model.js
+++ model.js
@@ -137,8 +137,10 @@ $("#numberOfShipsButton").click(function(){
 $("#fireButton").click(function(){
       var fireGuess = $("#guessInput").val();
       controller.processGuess(fireGuess)
-      fireGuess = ''
+      $("#guessInput").val('')
     });
+
+    
 
 var view = {
     displayMessage : function(msg){
